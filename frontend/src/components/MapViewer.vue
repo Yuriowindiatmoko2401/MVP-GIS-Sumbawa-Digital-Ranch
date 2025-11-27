79287f4a0ae577e5799a0cdc13f3dd17128d5c0b
fahrihshiddiq-ijita <fahrihshiddiq@gmail.com>
2025-11-28 00:21:14 +0700

    all running well , but have many error , all function not working properly

--- frontend/src/components/MapViewer.vue
+++ frontend/src/components/MapViewer.vue
@@ -80,12 +80,12 @@ import { ref, onMounted, onUnmounted, watch, nextTick } from 'vue'
 import L from 'leaflet'
 import 'leaflet/dist/leaflet.css'
 
-// Fix Leaflet default icon issue with webpack
+// Fix Leaflet default icon issue with Vite
 delete L.Icon.Default.prototype._getIconUrl
 L.Icon.Default.mergeOptions({
-  iconRetinaUrl: require('leaflet/dist/images/marker-icon-2x.png'),
-  iconUrl: require('leaflet/dist/images/marker-icon.png'),
-  shadowUrl: require('leaflet/dist/images/marker-shadow.png'),
+  iconRetinaUrl: '/assets/leaflet/images/marker-icon-2x.png',
+  iconUrl: '/assets/leaflet/images/marker-icon.png',
+  shadowUrl: '/assets/leaflet/images/marker-shadow.png',
 })
 
 export default {
